8828ccbd9f1a009426a5b207bd5fee1bbd782478
Nishant Shingate <nishantks121@gmail.com>
2025-12-02 20:28:00 +0530

    feat: added factory design pattern base classes

---
+++ FactoryDesignPattern/Circle.java
@@ -0,0 +1,12 @@
+public class Circle implements Shape {
+
+    @Override
+    public void computeArea() {
+        System.out.println("Inside Circle:: computeArea() method");
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Inside Circle:: draw() method");
+    }
+}
---
+++ FactoryDesignPattern/Shape.java
@@ -0,0 +1,4 @@
+public interface Shape {
+    void computeArea();
+    void draw();
+}
